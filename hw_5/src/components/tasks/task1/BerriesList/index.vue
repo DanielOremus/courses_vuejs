4718351f56dd5047eef98100d44d86db4eb81ec0
Daniel <oremusd89@gmail.com>
2025-02-01 10:00:02 +0200

    hw5, task1: minor fixes

--- hw_5/src/components/tasks/task1/BerriesList/index.vue
+++ hw_5/src/components/tasks/task1/BerriesList/index.vue
@@ -18,24 +18,7 @@ export default {
   props: {
     berriesList: {
       type: Array,
-      default: () => [
-        {
-          id: "1",
-          name: "Малина звичайна",
-          shopName: "Флоріум",
-          isAvailable: true,
-          imgSrc:
-            "https://img.7dach.ru/image/600/17/79/71/2017/04/21/3c4d32.jpg",
-        },
-        {
-          id: "2    ",
-          name: "Малина1111",
-          shopName: "FOZZY",
-          isAvailable: false,
-          imgSrc:
-            "https://img.7dach.ru/image/600/17/79/71/2017/04/21/3c4d32.jpg",
-        },
-      ],
+      default: () => [],
     },
   },
 }
--- hw_5/src/components/tasks/task1/index.vue
+++ hw_5/src/components/tasks/task1/index.vue
@@ -1,18 +1,47 @@
 <template>
   <div class="condition-container">Реалізувати список з ягодами</div>
   <div class="main">
-    <berries-list></berries-list>
+    <berries-list :berries-list="berriesList"></berries-list>
   </div>
 </template>
 
 <script>
+const berriesList = [
+  {
+    id: "1",
+    name: "Малина свіжа на вагу",
+    shopName: "Флоріум",
+    isAvailable: true,
+    imgSrc: "https://img.7dach.ru/image/600/17/79/71/2017/04/21/3c4d32.jpg",
+  },
+  {
+    id: "2",
+    name: "Малина великоплідна",
+    shopName: "FOZZY",
+    isAvailable: false,
+    imgSrc:
+      "https://cdn-img.perekrestok.ru/i/400x400-fit/xdelivery/files/6d/f6/27d1c1d1f14c35a31a4527a551b1.jpg",
+  },
+  {
+    id: "3",
+    name: "Ожина",
+    shopName: "Ягідна Мрія",
+    isAvailable: true,
+    imgSrc:
+      "https://soncesad.com/assets/images/products/14727/ozhina-svit-paj2.jpg",
+  },
+]
 import BerriesList from "./BerriesList/index.vue"
 export default {
   name: "Task1",
   components: {
     BerriesList,
   },
-  computed: {},
+  computed: {
+    berriesList() {
+      return berriesList
+    },
+  },
 }
 </script>
 
